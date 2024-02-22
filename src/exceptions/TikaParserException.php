c6d8b5053a6fadd75d745564b78ba33dd6a87726
Matt Marjanovic <maddog@mir.com>
2024-02-22 14:52:39 -0800

    dev: Define a couple of new TATF-specific checked exceptions

---
+++ src/exceptions/TikaParserException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of TikaAllTheFiles.
+ *
+ * Copyright 2024 Matt Marjanovic
+ *
+ * TikaAllTheFiles is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or any
+ * later version.
+ *
+ * TikaAllTheFiles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with TikaAllTheFiles.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @file
+ */
+
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\TikaAllTheFiles\Exceptions;
+
+
+class TikaParserException extends \Exception {
+}
